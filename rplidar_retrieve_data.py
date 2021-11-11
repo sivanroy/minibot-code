7da4103902cafd50b28b90629ea36a5358c589a5
Simon <vanroy.simon@outlook.com>
2021-11-11 11:59:59 +0100

    Update rplidar_retrieve_data.py

--- rplidar_retrieve_data.py
+++ rplidar_retrieve_data.py
@@ -6,23 +6,15 @@ lidar = RPLidar('/dev/ttyUSB0')
 info = lidar.get_info()
 print(info)
 
+
 def scan_lidar():
     for i, scan in enumerate(lidar.iter_scans()): #each scan = [quality,angle,dist]
-        theta_max = 0
-        dist_max = 0
-        i = 0
         prev_angle = 0
-        
-        
-        while (i < len(scan)):
-            (quality,angle,dist) = scan[i]
-            i+=1
-            error = 20
-            if(prev_angle-angle > 1): # after one tour, do smthg
-                dist_max = 0
-                theta_max = 0  
-            else: #not a tour done
-                if (dist_max < dist):
-                    dist_max = dist
-                    theta_max = angle 
-            prev_angle = angle
+        result = [[],[]]  #[[dist][angle]]
+        (quality,angle,dist) = scan[i]
+        if(prev_angle-angle > 100 ): # after one tour, do smthg
+            return result
+        else: #not a tour done
+            result[0].append(dist)
+            result[1].append(angle)
+        prev_angle = angle
