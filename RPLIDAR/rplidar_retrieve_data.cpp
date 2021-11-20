4619ccf3faf0acd9ea9d084f5236e4f7f466de0e
Simon <vanroy.simon@outlook.com>
2021-11-20 19:13:36 +0100

    Update rplidar_retrieve_data.cpp

--- RPLIDAR/rplidar_retrieve_data.cpp
+++ RPLIDAR/rplidar_retrieve_data.cpp
@@ -48,9 +48,20 @@
     //lidar_drv->getAllSupportedScanModes(scanModes);
 
     sl::LidarScanMode scanMode;//normal scan
+
     (*lidar)->startScan(false, true, 0, &scanMode);
 
     //grab scan data
+    sl_lidar_response_measurement_node_hq_t nodes[8192];
+    size_t nodeCount = sizeof(nodes)/sizeof(sl_lidar_response_measurement_node_hq_t);
+    res = lidar->grabScanDataHq(nodes, nodeCount);
+
+    if (IS_FAIL(res))
+    {
+        int i = 0;// failed to get scan data
+    }
+
+
     // TODO
 
     //lidar->stopMotor();	
