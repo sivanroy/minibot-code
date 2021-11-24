9a71ec7e66b6e041721fa4cd12d48e8793e648f8
Simon <vanroy.simon@outlook.com>
2021-11-24 15:25:43 +0100

    sub files

--- Python/test_demo.py
+++ Python/test_demo.py
@@ -2,71 +2,99 @@ from rplidar import RPLidar
 import RPi.GPIO as GPIO
 import time
 import numpy as np
-from Displacement import motors, MAX_SPEED
+from displacement import motors, MAX_SPEED
 
 MAXDIST = 600
 MINDIST = 300
 ERROR = 20
 speed = 0.5
 
+#begin motors
 motors.start_all()
 
+#begin lidars
 lidar = RPLidar('/dev/ttyUSB0')
+lidar.start()
+lidar.start_motor()
 info = lidar.get_info()
 print(info)
 
-for i, lidar_scan in enumerate(lidar.iter_scans()):
-    theta = []
-    dist = []
-    # print('%d: Got %d measurments' % (i, len(lidar_scan)))
-    for scan in (lidar_scan):  # each scan = [quality,angle,dist]
-        theta.append(scan[1])
-        dist.append(scan[2])
+
+def controllers(scan_data):
+    dist = scan_data[0]
+    theta = scan_data[1]
     max_index = np.where(dist == np.amin(np.array(dist)))[0][0]
     dist_p = dist[max_index]
     theta_p = theta[max_index]
-
+    print("begin")
     # Balise trop proche -> stop
     if (dist_p < MINDIST or dist_p > MAXDIST):
         motors.stop_all()
-        # print('stop')
 
     # Balise à gauche
     elif (theta_p < 180 - ERROR):
-
         # Balise arriere gauche
         if (theta_p < 90 - ERROR):
             motors.set_speeds(-speed, speed)
-            # print('+ arr')
-
         # Balise avant gauche
         else:
             motors.set_speeds(0, speed)
-            # print('+ avt')
-
     # Balise à droite
     elif (theta_p > 180 + ERROR):
-
         # Balise arriere droite
         if (theta_p > 270 - ERROR):
             motors.set_speeds(speed, -speed)
-            # print('- arr')
-
         # Balise avant gauche
         else:
             motors.set_speeds(speed, 0)
-            # print('- avt')
-
     # Balise en face
     else:
         motors.set_speeds(speed, speed)
-        # print('straigth')
 
-"""
+#begin scan
+for i, lidar_scan in enumerate(lidar.iter_scans()):
+    #set scan data
+    theta = []; dist = []
+    for scan in (lidar_scan):  # each scan = [quality,angle,dist]
+        theta.append(scan[1])
+        dist.append(scan[2])
+
+    #controllers([dist,theta])
+    dist = scan_data[0]
+    theta = scan_data[1]
+    max_index = np.where(dist == np.amin(np.array(dist)))[0][0]
+    dist_p = dist[max_index]
+    theta_p = theta[max_index]
+    # Balise trop proche -> stop
+    if (dist_p < MINDIST or dist_p > MAXDIST):
+        motors.stop_all()
+
+    # Balise à gauche
+    elif (theta_p < 180 - ERROR):
+        # Balise arriere gauche
+        if (theta_p < 90 - ERROR):
+            motors.set_speeds(-speed, speed)
+        # Balise avant gauche
+        else:
+            motors.set_speeds(0, speed)
+
+    # Balise à droite
+    elif (theta_p > 180 + ERROR):
+        # Balise arriere droite
+        if (theta_p > 270 - ERROR):
+            motors.set_speeds(speed, -speed)
+        # Balise avant gauche
+        else:
+            motors.set_speeds(speed, 0)
+    # Balise en face
+    else:
+        motors.set_speeds(speed, speed)
+
+
 lidar.stop()
 lidar.stop_motor()
 lidar.disconnect()
-"""
+
 
 
 
