933f74efb991b6168ff25f91a7d0721d58552465
libertlo <louis.libert@student.uclouvain.be>
2021-12-06 16:54:15 +0100

    Update MySPI.py

--- Python/MySPI.py
+++ Python/MySPI.py
@@ -2,38 +2,44 @@ import RPi.GPIO as GPIO
 from time import sleep
 import spidev
 
-MyARM_ResetPin = 19 # Pin 4 of connector = BCM19 = GPIO[1]
 
 MySPI_FPGA = spidev.SpiDev()
-MySPI_FPGA.open(0,0)
-MySPI_FPGA.max_speed_hz = 50000
+MySPI_FPGA.open(0,1)
+MySPI_FPGA.max_speed_hz = 500000
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(MyARM_ResetPin, GPIO.OUT)
 
-GPIO.output(MyARM_ResetPin, GPIO.HIGH)
-sleep(0.1)
-GPIO.output(MyARM_ResetPin, GPIO.LOW)
-sleep(0.1)
 
-i = 0
-stop = 4000
-while i < stop:
+def count(spi):
+    treshold = 4192
+
+    return spi[4] + (spi[3] << 8) + (spi[2] << 16) + (spi[1] << 24) - treshold
+
+
+while 1:
     ToSPI_leftEnc = [0x00, 0x00, 0x00, 0x00, 0x00]
-    countLeftEnc = MySPI_FPGA.xfer2(ToSPI_leftEnc)
+    countLeftEnc = -1 * count(MySPI_FPGA.xfer2(ToSPI_leftEnc))
+    # -1 car à gauche on est en inversé
 
     ToSPI_rightEnc = [0x01, 0x00, 0x00, 0x00, 0x00]
-    countRightEnc = MySPI_FPGA.xfer2(ToSPI_rightEnc)
+    countRightEnc = count(MySPI_FPGA.xfer2(ToSPI_rightEnc))
 
     ToSPI_leftOdo = [0x02, 0x00, 0x00, 0x00, 0x00]
-    countLeftOdo = MySPI_FPGA.xfer2(ToSPI_leftOdo)
+    countLeftOdo = -1 * count(MySPI_FPGA.xfer2(ToSPI_leftOdo))
 
     ToSPI_rightOdo = [0x03, 0x00, 0x00, 0x00, 0x00]
-    countRightOdo = MySPI_FPGA.xfer2(ToSPI_rightOdo)
+    countRightOdo = count(MySPI_FPGA.xfer2(ToSPI_rightOdo))
+    
+    if (countLeftOdo != 0):
+        print("countLeftOdo =", countLeftOdo)
+    if (countRightOdo != 0):
+        print("countRightOdo =", countRightOdo)
+    if (countLeftEnc != 0):
+        print("countLeftEnc =", countLeftEnc)
+    if (countRightEnc != 0):
+        print("countRightEnc =", countRightEnc)
 
-    print("countLeftOdo =", countLeftOdo)
 
-    i += 1
 
 
